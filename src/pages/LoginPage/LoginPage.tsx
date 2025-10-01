185dad796e88a0db7bec1a41184c01d46cb15732
Pawsing-ctr <tnechayev@internet.ru>
2025-10-02 00:44:49 +0300

    fix

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import FooterHome from "../components/HomePage/ui/Footer/FooterHome";
-import Hero from "../components/HomePage/ui/Hero/Hero";
+import { PageBlockWrapper } from "../../components/LoginPage/ui/PageBlockWrapper/PageBlockWrapper";
+import AuthCard from "../../components/LoginPage/ui/AuthCard/AuthCard";
+import FooterHome from "../../components/HomePage/ui/Footer/FooterHome";
+import Hero from "../../components/HomePage/ui/Hero/Hero";
 
 const LoginPage = () => {
   return (
