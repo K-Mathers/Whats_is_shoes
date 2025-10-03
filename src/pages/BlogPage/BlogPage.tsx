f03f3ab5eec8b4dc2fb591cdbd15918374f1aa8e
Pawsing-ctr <tnechayev@internet.ru>
2025-10-03 18:13:00 +0300

    end blog page/ need background and font

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -1,16 +1,18 @@
 import "./BlogPage.css";
 import CategorieCard from "../../components/BlogPage/ui/CategorieCard/CategorieCard";
-import HeaderBLog from "../../components/BlogPage/ui/Header/HeaderBlog";
 import SearchBlock from "../../components/BlogPage/ui/SearchBlock/SearchBlock";
 import CreateArticle from "../../components/BlogPage/ui/CreateArticle/CreateArticle";
+import FooterHome from "../../components/HomePage/ui/Footer/FooterHome";
+import Hero from "../../components/HomePage/ui/Hero/Hero";
 
 const BlogPage = () => {
   return (
     <div className="blog-page">
-      <HeaderBLog />
+      <Hero backgroundColor="#f05060" />
       <SearchBlock />
       <CategorieCard />
       <CreateArticle />
+      <FooterHome />
     </div>
   );
 };
