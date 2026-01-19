7dde972c36a5dec7ecdbb3a5dd1e59ac7b879590
Kimerland <kimerland.project@gmail.com>
2026-01-19 22:09:44 +0300

    feat(uploads) created profile bar

--- src/pages/ProfilePage/ProfileCard/ProfileCard.tsx
+++ src/pages/ProfilePage/ProfileCard/ProfileCard.tsx
@@ -1,34 +1,80 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./ProfileCard.css";
-import { getUser } from "@/api/auth";
+import { changePassword, getUser, logoutUser, } from "@/api/auth";
+import { useNavigate } from "react-router-dom";
+
+const TAB_TITLES = {
+  general: "General",
+  security: "Security Settings",
+  forgotPass: "Password Recovery",
+  verifEmail: "Email Verification",
+  logout: "Sign Out",
+};
 
 const ProfileCard = () => {
   const [isEditing, setIsEditing] = useState(false);
-  const [activeTab, setActiveTab] = useState("general");
+  const [activeTab, setActiveTab] = useState<keyof typeof TAB_TITLES>("general");
   const [formData, setFormData] = useState({
     email: "",
     role: "",
     createdAt: "",
     isVerified: "",
   });
-
+  const [forgotEmail, setForgotEmail] = useState("");
   const [passwordData, setPasswordData] = useState({
     currentPassword: "",
     newPassword: "",
     confirmPassword: "",
   });
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser();
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleVerification = async () => { };
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const data = await getUser();
         setFormData(data);
+        setForgotEmail(data.email);
       } catch (err) {
         console.error(err);
       }
     };
     fetchUserData();
   }, []);
+
+  const handleSaveResetPass = async () => {
+    const { currentPassword, newPassword, confirmPassword } = passwordData;
+    if (!currentPassword || !newPassword || !confirmPassword) {
+      alert("Enter all");
+      return;
+    }
+
+    if (newPassword !== confirmPassword) {
+      alert("Passwords don't exit's");
+      return;
+    }
+
+    try {
+      await changePassword({ currentPassword, newPassword });
+      setPasswordData({
+        currentPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div className="profile-page">
@@ -48,14 +94,34 @@ const ProfileCard = () => {
         >
           Security
         </button>
+
+        <button
+          className={`profile-sidebar__btn ${activeTab === "forgotPass" ? "profile-sidebar__btn--active" : ""}`}
+          onClick={() => setActiveTab("forgotPass")}
+        >
+          Forgot Password
+        </button>
+
+        <button
+          className={`profile-sidebar__btn ${activeTab === "verifEmail" ? "profile-sidebar__btn--active" : ""}`}
+          onClick={() => setActiveTab("verifEmail")}
+        >
+          Verification Email
+        </button>
+
+        <button
+          className={`profile-sidebar__btn ${activeTab === "logout" ? "profile-sidebar__btn--active" : ""}`}
+          onClick={() => setActiveTab("logout")}
+        >
+          Logout
+        </button>
       </aside>
 
       <main className="profile-content">
         <div className="profile-card">
           <div className="profile-card__header">
-            <h2 className="profile-card__title">
-              {activeTab === "general" ? "Profile" : "Settings"}
-            </h2>
+            <h2 className="profile-card__title">{TAB_TITLES[activeTab]}</h2>
+            {activeTab === "general"}
           </div>
 
           {activeTab === "general" && (
@@ -69,7 +135,6 @@ const ProfileCard = () => {
                   disabled={!isEditing}
                 />
               </div>
-
               <div className="profile-form__group">
                 <label className="profile-form__label">Role</label>
                 <input
@@ -79,7 +144,6 @@ const ProfileCard = () => {
                   disabled={!isEditing}
                 />
               </div>
-
               <div className="profile-form__group">
                 <label className="profile-form__label">Created At</label>
                 <input
@@ -108,10 +172,15 @@ const ProfileCard = () => {
                 <label className="profile-form__label">Current Password</label>
                 <input
                   type="password"
-                  name="currentPassword"
                   className="profile-form__input"
                   placeholder="******"
-                  // onChange={handleInputChange}
+                  value={passwordData.currentPassword}
+                  onChange={(e) =>
+                    setPasswordData({
+                      ...passwordData,
+                      currentPassword: e.target.value,
+                    })
+                  }
                 />
               </div>
 
@@ -119,9 +188,14 @@ const ProfileCard = () => {
                 <label className="profile-form__label">New Password</label>
                 <input
                   type="password"
-                  name="newPassword"
                   className="profile-form__input"
-                  // onChange={handleInputChange}
+                  value={passwordData.newPassword}
+                  onChange={(e) =>
+                    setPasswordData({
+                      ...passwordData,
+                      newPassword: e.target.value,
+                    })
+                  }
                 />
               </div>
 
@@ -129,19 +203,39 @@ const ProfileCard = () => {
                 <label className="profile-form__label">Confirm Password</label>
                 <input
                   type="password"
-                  name="confirmPassword"
                   className="profile-form__input"
-                  // onChange={handleInputChange}
+                  value={passwordData.confirmPassword}
+                  onChange={(e) =>
+                    setPasswordData({
+                      ...passwordData,
+                      confirmPassword: e.target.value,
+                    })
+                  }
                 />
               </div>
 
               <div className="profile-actions">
                 <button
                   className="profile-btn profile-btn--primary"
-                  // onClick={handleSave}
+                  onClick={handleSaveResetPass}
                 >
                   Update Password
                 </button>
+              </div>
+            </div>
+          )}
+
+          {activeTab === "forgotPass" && (
+            <div className="profile-form profile-form--full">
+              <div className="profile-form__group">
+                <label className="profile-form__label">
+                  Lost your password? Enter your email address.
+                </label>
+                <input
+                  type="password"
+                  className="profile-form__input"
+                  placeholder="******"
+                />
               </div>
             </div>
           )}
